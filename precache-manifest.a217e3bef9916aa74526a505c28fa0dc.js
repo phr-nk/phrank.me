1ab1c3a3b6822478fb0431d098809d33f7cd10de
Phrank Lenoci <fcl1118@live.com>
2020-04-26 16:40:53 -0500

    Updates

---
+++ precache-manifest.a217e3bef9916aa74526a505c28fa0dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd43066a29285cdf2200b858da876cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e777b99fde20f5ea57",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0866bf7d0b54d76423e7",
+    "url": "/static/js/2.aa5e5d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa5e5d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e777b99fde20f5ea57",
+    "url": "/static/js/main.677a5e33.chunk.js"
+  },
+  {
+    "revision": "e9917f1ef534fb305278",
+    "url": "/static/js/runtime-main.4649510f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
