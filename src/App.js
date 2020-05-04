5bb0b4645995a2c088d62a5878c052bca4b467c3
Phrank Lenoci <fcl1118@live.com>
2020-05-03 19:13:26 -0500

    face bust included

--- src/App.js
+++ src/App.js
@@ -1,33 +1,91 @@
 import React from 'react';
 import logo from './logo.svg';
 import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import './App.css';
 
 class App extends React.Component {
   componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
+    // Create Scene + Camera
+
+const mainScene = new THREE.Scene()
+
+const mainCamera = new THREE.PerspectiveCamera(
+  3,
+  window.innerWidth / window.innerHeight,
+  1,
+  50
+)
+mainCamera.position.z = 10
+mainCamera.position.y = 0.3
+// Add Point Lights
+
+const backLight = new THREE.PointLight(0x00aaff, 3, 20)
+backLight.position.set(-5, 5, -5)
+mainScene.add(backLight)
+
+const fillLight = new THREE.PointLight(0x00aaff, 0.7, 20)
+fillLight.position.set(-5, 0, 5)
+mainScene.add(fillLight)
+
+const keyLight = new THREE.PointLight(0xff00ff, 2, 20)
+keyLight.position.set(5, 0, 0)
+mainScene.add(keyLight)
+
+// Create Renderer
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+// Load 3D Model
+
+const loader = new GLTFLoader()
+//const modelFile = require('./scene.glb')
+
+const modelContainer = new THREE.Group()
+mainScene.add(modelContainer)
+
+loader.load(
+  "./scene.glb",
+  gltf => {
+    modelContainer.add(gltf.scene)
+  },
+  undefined,
+  console.error
+)
+
+// Handle Window Resize
+
+function resizeRenderer() {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  mainCamera.aspect = window.innerWidth / window.innerHeight
+  mainCamera.updateProjectionMatrix()
+}
+//window.addEventListener('resize', debounce(resizeRenderer, 50))
+
+// Render Scene
+
+const clock = new THREE.Clock()
+
+function render() {
+  const delta = clock.getDelta()
+
+
+  modelContainer.rotation.y += delta * 0.5
+
+  
+  renderer.render(mainScene, mainCamera)
+  
+  requestAnimationFrame(render)
+}
+render()
     // === THREE.JS EXAMPLE CODE END ===
   }
   render() {
     return (
-      <div />
+      <div>
+      </div>
     )
   }
 }
