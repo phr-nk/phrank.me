6604dd2637bd956a90484a09e86c0679006dd81f
Phrank Lenoci <fcl1118@live.com>
2020-07-04 11:29:54 -0500

    Updates

---
+++ precache-manifest.2fd502806c39e0f46fd3368ef615ccd0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d084605b94640d84e5b0e9e6508931e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401e2ec9957db8b27fe5",
+    "url": "/static/css/main.9f97bebf.chunk.css"
+  },
+  {
+    "revision": "442309d4e7f5eff6b9a0",
+    "url": "/static/js/2.cfe365df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cfe365df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401e2ec9957db8b27fe5",
+    "url": "/static/js/main.3563fa32.chunk.js"
+  },
+  {
+    "revision": "e9917f1ef534fb305278",
+    "url": "/static/js/runtime-main.4649510f.js"
+  },
+  {
+    "revision": "ca3e1023ea85c8b36554eb1f8479ff0a",
+    "url": "/static/media/Frank_Lenoci_Resume_2020_P.ca3e1023.pdf"
+  },
+  {
+    "revision": "620abc4ef283f67bfc3ecd69660110fd",
+    "url": "/static/media/face.620abc4e.png"
+  },
+  {
+    "revision": "31b44d61497694f8006ef1a5d02709b3",
+    "url": "/static/media/github_icon.31b44d61.png"
+  },
+  {
+    "revision": "6e2e7e644a433618836e6a1000ad8d1b",
+    "url": "/static/media/gmail_icon.6e2e7e64.png"
+  },
+  {
+    "revision": "62c119da233402541445cee14c9f9d2b",
+    "url": "/static/media/hacked-kerx-webfont.62c119da.woff"
+  },
+  {
+    "revision": "a3554beebe32a2bef7488d462dd1e4d8",
+    "url": "/static/media/hacked-kerx-webfont.a3554bee.woff2"
+  },
+  {
+    "revision": "8b0cf889fa8f30025609db5cecd9909e",
+    "url": "/static/media/linkedin_icon.8b0cf889.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad1567da03ea7156de94d77a73870acf.js"
+  "/precache-manifest.2fd502806c39e0f46fd3368ef615ccd0.js"
 );
 
 self.addEventListener('message', (event) => {
